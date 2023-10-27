92583be2dc39c4fd03a2ecbf99753773723a1d01
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-10-28 01:46:46 +0530

    cam using

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ const constraints = {
         height:571
     }
 };
+
+async function init(){
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+    }
+    catch(e){
+        errorMsgElement.innerHTML = `navigator.getUserMedia.error:${e.toString()}`;
+    }
+}
+
+function handleSuccess(stream){
+    window.stream = stream;
+    video.srcObject = stream;
+}
+
+init();
+
+var context = canvas.getContext('2d');
+snap.addEventListener("click",function(){
+    context.drawImage(video, 0 , 350, 571);
+});
